1c3cfa84cd2bc244198a3641a514ef8f6fa09d08
Jillian Shirai <jtshirai@hawaii.edu>
2026-02-10 17:17:52 -1000

    Completed debugging of the IBoutlets and Actions, couldn't get simulator to load, something wrong in the app delegate, will ask in class

--- XcodeTroubleshootingAssignment/SecondViewController2.swift
+++ XcodeTroubleshootingAssignment/SecondViewController2.swift
@@ -9,35 +9,54 @@
 import UIKit
 
 class SecondViewController2: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         // Do any additional setup after loading the view.
     }
     
-
+    
     @IBAction func firstButtonPressed(_ sender: Any) {
     }
     /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destination.
+     // Pass the selected object to the new view controller.
+     }
+     */
     @IBAction func secondButtonPressed(_ sender: Any) {
     }
     
+    @IBOutlet weak var colorChange: UIButton!
     
-    @IBAction func changeColorButtonPressed(_ sender: Any) {
-    } self.view.backgroundcolor = color.blue
+    @IBAction func changeColorPressed(_ sender: UIButton) {
+        view.backgroundColor = .blue
+    }
     
+    
+    
+    @IBOutlet weak var changeLabelButton: UIButton!
+    
+    
+    @IBOutlet weak var firstLabel: UILabel!
+
+    @IBAction func changeLabelTextButtonPressed(_ sender: UIButton) {
+  
+        firstLabel.text = "I feel confident with the Xcode troubleshooting process"
+   
+        
+        
+    }
     
     
 }
-@IBAction func changeLabelTextButtonPressed(_ sender: Any) {
-    firstLabel.text = I feel confident with the Xcode troubleshooting process"
-}
+
+
+
+
+
+
